1a5f6b4ccf90344c6f8d66e61fffd9d2beac7061
Keshav Gupta <140381004+officialkeshav@users.noreply.github.com>
2025-01-10 20:09:45 +0530

    voice.py

--- voice.py
+++ voice.py
@@ -106,7 +106,7 @@ if __name__ == "__main__":
             try:
                 speak("What should I say?")
                 content = takeCommand()
-                to = "harryyourEmail@gmail.com"    
+                to = "abc@gmail.com"    
                 sendEmail(to, content)
                 speak("Email has been sent!")
             except Exception as e:
